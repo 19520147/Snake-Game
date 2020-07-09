c876fa86d060c597c11d0e1a3c3ca9790eb7320b
19520147 <67216876+19520147@users.noreply.github.com>
2020-07-09 10:01:55 +0700

    Update snake.cpp
    Vẽ tường rắn

--- snake.cpp
+++ snake.cpp
@@ -2,6 +2,31 @@
 #include"hdrlib.h"
 using namespace std;
 int n = 4;
+void drawWall()
+{
+	// 10 <= x <= 100
+	// 1 <= y <= 26
+	SetColor(11);
+	for (int x = 10; x < 101; x++)
+	{
+		int y = 1;
+		int z = 26;
+		gotoXY(x, y);
+		cout << "+";
+		gotoXY(x, z);
+		cout << "+";
+	}
+	for (int y=1; y<27; y++)
+	{
+		int x = 100;
+		int z = 10;
+		gotoXY(x, y);
+		cout << "+";
+		gotoXY(z, y);
+		cout << "+";
+	}
+	SetColor(7);
+}
 void drawSnake(int x[], int y[])
 {
 	gotoXY(x[0], y[0]);
